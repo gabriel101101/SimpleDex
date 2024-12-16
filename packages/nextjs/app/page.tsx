782803eeac4673024be9fe41999cf5158ed55b7a
gaby_0101 <gabyx10666.gmail.com>
2024-12-16 11:35:41 -0300

    dep02

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -18,8 +18,8 @@ const Home: NextPage = () => {
       <div className="card bg-base-100 w-96 shadow-xl">
   
   <div className="card-body">
-    <h2 className="card.title">Para ver los TOKENS</h2>
-    <h2 className="card-title">Ingrese en DEBUG CONTRACT</h2>
+    <h2 className="card-title"></h2>
+    <h2 className="card-title">Ingrese en DEBUG CONTRACTS</h2>
   </div>
 </div>
       
